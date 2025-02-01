f3889cd4b17a1a6f83c263ea78b5cd972bc9ac46
zhangir003 <zhangir003@mail.ru>
2025-02-02 00:19:47 +0500

    InitServiceReader

--- LibaryAPI/LibaryAPI.Application/Services/ReadersService/ReaderService.cs
+++ LibaryAPI/LibaryAPI.Application/Services/ReadersService/ReaderService.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using LibaryAPI.Domain.DTOs.Readers;
 using LibaryAPI.Domain.Interfaces;
 using LibaryAPI.Infrastructure.Models.Readers;
 using LibaryAPI.Infrastructure.Repositories;
@@ -11,6 +12,43 @@ public class ReaderService : AbstractService<IReaderRepository, ReaderModel, IGe
 {
     public ReaderService(ILogger logger, IMapper mapper, IReaderRepository repository) : base(logger, mapper, repository)
     {
+    }
+
+    public async Task<IEnumerable<GetReaderDto>> GetReadersAsync()
+    {
+        return mapper.Map<IEnumerable<GetReaderDto>>(await _repository.GetAllAsync());
+    }
+
+    public async Task<GetReaderDto> GetReaderByIdAsyncd(int id)
+    {
+        return mapper.Map<GetReaderDto>(_repository.GetByIdAsync(id));
+    }
+
+    public async Task<GetReaderDto> CreateReaderAsync(CreateReaderDto create)
+    {
+        var reader = mapper.Map<ReaderModel>(create);
+        var result = await _repository.CreateAsync(reader);
+        return mapper.Map<GetReaderDto>(result);
+    }
+
+    public async Task<GetReaderDto> UpdateReaderAsync(UpdateReaderDto update)
+    {
+        var reader = await _repository.GetAsync(mapper.Map<ReaderModel>(update));
+        if(reader != null)
+        {
+            reader.FullName = update.FullName;
+            reader.AccountNumber = update.AccountNumber;
+            reader.DateOfBirth = update.DateOfBirth;
+        }
+        var result = mapper.Map<GetReaderDto>(await _repository.UpdateAsync(reader));
+        return result;
+    }
+
+    public async Task<GetReaderDto> DeleteReaderAsync(int id)
+    {
+        var reader = mapper.Map<ReaderModel>(_repository.GetByIdAsync(id));
+        var result = await _repository.DeleteAsync(reader);
+        return mapper.Map<GetReaderDto>(result);
     }
 }
 

2c0ef5bc0b933f6057eb41b8e5ae3eee8fc565e1
zhangir003 <zhangir003@mail.ru>
2025-02-01 11:18:32 +0500

    InitInfrastructureModels

---
+++ LibaryAPI/LibaryAPI.Infrastructure/Models/Readers/ReadersModel.cs
@@ -0,0 +1,8 @@
+﻿namespace LibaryAPI.Infrastructure.Models.Readers;
+public class ReadersModel:BaseModel
+{ 
+    public string FullName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public int AccountNumber { get; set; }
+}
+

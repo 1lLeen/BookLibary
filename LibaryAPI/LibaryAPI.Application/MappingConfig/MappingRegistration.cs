a281659fb9933fce378cc2c11e716927fa438969
zhangir003 <zhangir003@mail.ru>
2025-02-02 17:39:49 +0500

    AddMappingProfile

---
+++ LibaryAPI/LibaryAPI.Application/MappingConfig/MappingRegistration.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace LibaryAPI.Application.MappingConfig;
+
+public static class MappingRegistration
+{
+    public static void RegistrationAutoMapper(this IServiceCollection services)
+    {
+        services.AddAutoMapper(typeof(MappingProfile));
+    }
+}
